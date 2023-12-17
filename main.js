5580e2569f04108321146f638adce2c14fc903c3
Charan <charankulal0241@gmail.com>
2023-12-17 14:32:08 +0530

    Initial Commit

---
+++ main.js
@@ -0,0 +1,12 @@
+var typed = new Typed(".text", {
+    strings: [
+      "Full Stack Developer",
+      "Android Developer",
+      "Web Developer",
+      "Python Developer",
+    ],
+    typeSpeed: 10,
+    backSpeed: 10,
+    backDelay: 550,
+    loop: true,
+  });
